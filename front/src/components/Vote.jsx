8b9405a1b0ef168913d2c467faf23dc721d8c26d
Platon <platon20081019@gmail.com>
2024-12-08 23:15:50 +0200

    CZ

--- front/src/components/Vote.jsx
+++ front/src/components/Vote.jsx
@@ -15,16 +15,19 @@ const Vote = () => {
   };
 
   return (
-    <div>
+    <>
       <h2 className="mt-7">Голосование</h2>
-      <input
-        type="number"
-        placeholder="ID кандидата"
-        value={candidateId}
-        onChange={(e) => setCandidateId(e.target.value)}
-      />
-      <button onClick={handleVote}>Голосовать</button>
-    </div>
+      <div className="flex gap-10">
+        <input
+          type="number"
+          placeholder="ID кандидата"
+          value={candidateId}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-3"
+          onChange={(e) => setCandidateId(e.target.value)}
+        />
+        <button onClick={handleVote}>Голосовать</button>
+      </div>
+    </>
   );
 };
 
