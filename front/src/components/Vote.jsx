f12d817f047822b8688eb9d558744f6f626f77a4
Platon <platon20081019@gmail.com>
2025-02-16 23:26:14 +0200

    фіва

--- front/src/components/Vote.jsx
+++ front/src/components/Vote.jsx
@@ -61,13 +61,22 @@ const Vote = () => {
   return (
     <>
       <h2 className="mt-7">Голосування</h2>
-      <input
-        type="number"
-        placeholder="Indetefication"
-        value={identification}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        onChange={(e) => setIdentification(e.target.value)}
-      />
+      <div className="flex gap-10 mt-3">
+        <input
+          type="number"
+          placeholder="Indetefication"
+          value={uniqueCode}
+          onChange={(e) => setUniqueCode(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+        <button
+          onClick={handleVote}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Перевірити валідність коду
+        </button>
+      </div>
+
       <div className="flex gap-10 mt-3">
         <input
           type="number"
