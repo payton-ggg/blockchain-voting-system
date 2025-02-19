0c301686477b2c4a5a240e309ace3501212cc518
Platon <platon20081019@gmail.com>
2025-02-19 22:41:27 +0200

    asfas

--- front/src/components/Vote.jsx
+++ front/src/components/Vote.jsx
@@ -38,7 +38,7 @@ const Vote = () => {
           placeholder="Idetefication"
           value={uniqueCode}
           onChange={(e) => setUniqueCode(e.target.value)}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-[#7C7F65] border border-green-700 placeholder-gray-800 text-black text-sm rounded-lg block w-full p-2.5"
         />
       </div>
 
@@ -47,12 +47,12 @@ const Vote = () => {
           type="number"
           placeholder="ID кандидата"
           value={selectedCandidate}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-[#7C7F65] border border-green-700 placeholder-gray-800 text-black text-sm rounded-lg block w-full p-2.5"
           onChange={(e) => setSelectedCandidate(e.target.value)}
         />
         <button
           onClick={handleVote}
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="hover:bg-[#BEA8AA] focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
           Голосувати
         </button>
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="add-candidates" element={<></>} />
+        <Route path="list-of-candidates" element={<></>} />
+        <Route path="voting" element={<></>} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
