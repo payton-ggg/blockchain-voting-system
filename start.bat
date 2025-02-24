c708487482c0bf6bae03136f40325bbbc5725b7a
Platon <platon20081019@gmail.com>
2025-02-24 17:20:45 +0200

    readme

--- start.bat
+++ start.bat
@@ -1,13 +1,10 @@
 @echo off
-echo Запуск Ganache...
-start "Ganache" cmd /k ganache-cli -p 7545
-
-timeout /t 5 /nobreak >nul
-
 echo Запуск backend...
-start "Backend" cmd /k cd back && npm start
+start "" cmd /k "cd /d back && npm start"
+
+timeout /t 2 /nobreak >nul
 
 echo Запуск frontend...
-start "Frontend" cmd /k cd front && npm run dev
+start "" cmd /k "cd /d front && npm run dev"
 
 echo Все процессы запущены!
